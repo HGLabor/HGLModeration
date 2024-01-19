4c39cee0669e384d93538eaafb2232f49e9ac984
togarAragot <the.aragot@gmail.com>
2024-01-19 20:07:46 +0100

    initial Commit

---
+++ src/main/java/me/aragot/hglmoderation/HGLModeration.java
@@ -0,0 +1,23 @@
+package me.aragot.hglmoderation;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "hglmoderation",
+        name = "HGLModeration",
+        version = "1.0-SNAPSHOT",
+        authors = {"Aragot"}
+)
+public class HGLModeration {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
