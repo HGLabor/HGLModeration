c2b95b6e61ba6a9c659ab2a8ae19c43e62fdb9ca
togarAragot <the.aragot@gmail.com>
2024-01-27 20:40:38 +0100

    Added check for self-report and added clickable Report Submission for the chat

--- src/main/java/me/aragot/hglmoderation/commands/ReportCommand.java
+++ src/main/java/me/aragot/hglmoderation/commands/ReportCommand.java
@@ -5,14 +5,16 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
-import com.velocitypowered.api.command.VelocityBrigadierMessage;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
-import me.aragot.hglmoderation.data.reports.Reasoning;
+import me.aragot.hglmoderation.data.Reasoning;
+import me.aragot.hglmoderation.data.reports.Report;
 import me.aragot.hglmoderation.response.Responder;
 import me.aragot.hglmoderation.response.ResponseType;
 import me.aragot.hglmoderation.tools.StringUtils;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class ReportCommand {
@@ -36,16 +38,20 @@ public class ReportCommand {
 
                             return builder.buildFuture();
                         })
-                        //execute when /report <arg>
+
                         .executes(context -> {
                             String reported = context.getArgument("player", String.class);
-                            Player player = context.getSource() instanceof Player ? (Player) context.getSource() : null;
-                            if(player == null) return BrigadierCommand.FORWARD;
+                            Player reporter = context.getSource() instanceof Player ? (Player) context.getSource() : null;
+                            if(reporter == null) return BrigadierCommand.FORWARD;
 
-                            if(!isValidPlayer(server, player, reported))
+                            if(!isValidPlayer(server, reporter, reported))
                                 return Command.SINGLE_SUCCESS;
 
-                            //Open report book here
+                            if(server.getPlayer(reported).get().equals(reporter)){
+                                Responder.respond(reporter, "Sorry, but you cannot report yourself!", ResponseType.ERROR);
+                            }
+
+                            reportSuggestion(reporter, reported);
 
                             return Command.SINGLE_SUCCESS;
                         })
@@ -55,29 +61,34 @@ public class ReportCommand {
                                     builder.suggest(StringUtils.capitalize(reason.toString().toLowerCase()));
                                 return builder.buildFuture();
                             })
+
                             //executes when /report playerName reason
                             .executes(context -> {
                                 String reported = context.getArgument("player", String.class);
-                                Player player = context.getSource() instanceof Player ? (Player) context.getSource() : null;
-                                if(player == null) return BrigadierCommand.FORWARD;
+                                Player reporter = context.getSource() instanceof Player ? (Player) context.getSource() : null;
 
-                                if(!isValidPlayer(server, player, reported))
+                                if(reporter == null) return BrigadierCommand.FORWARD;
+
+                                if(!isValidPlayer(server, reporter, reported))
                                     return Command.SINGLE_SUCCESS;
+                                Player reportedPlayer = server.getPlayer(reported).get();
+
+                                if(reportedPlayer.equals(reporter)){
+                                    Responder.respond(reporter, "Sorry, but you cannot report yourself!", ResponseType.ERROR);
+                                }
 
                                 String reasoning = context.getArgument("reasoning", String.class);
 
                                 try {
-                                    Reasoning reason = Reasoning.valueOf(reasoning);
-
+                                    Reasoning reason = Reasoning.valueOf(reasoning.toUpperCase());
+                                    Report.submitReport(reportedPlayer.getUniqueId().toString(), reporter.getUniqueId().toString(), reason, Report.getPriorityForReporter(reporter));
+                                    Responder.respond(reporter, "Your report has been submitted. Our team will review your report as soon as possible. Thank you for your patience!", ResponseType.SUCCESS);
                                 } catch (IllegalArgumentException x) {
-                                    //Open report book here as well
+                                    reportSuggestion(reporter, reported);
                                 }
-
                                 return Command.SINGLE_SUCCESS;
                             }))
                 )
-
-                //Second Argument for reasoning missing
 
                 .build();
         return new BrigadierCommand(reportNode);
@@ -89,5 +100,21 @@ public class ReportCommand {
         Responder.respond(sender, "The player '" + reported + "' is currently not online.", ResponseType.ERROR);
 
         return false;
+    }
+
+    private static void reportSuggestion(Player player, String reportedName){
+        MiniMessage mm = MiniMessage.miniMessage();
+        Component reportText = mm.deserialize(Responder.prefix + " You are about to report <b><red>" + reportedName + "</red></b>.<br>" + "Please pick a reasoning for the report:<br><br>");
+
+        for(Reasoning reason : Reasoning.values()){
+            String name = StringUtils.capitalize(reason.name().toLowerCase());
+            reportText = reportText.append(
+                    mm.deserialize("   <gray>☉</gray><red> " + name + "</red>")
+                            .clickEvent(ClickEvent.runCommand("/report " + reportedName + " " + name))
+                            .hoverEvent(HoverEvent.showText(mm.deserialize("<red>Report </red>" + reportedName + " <red>for " + name)))
+            ).appendNewline();
+        }
+
+        player.sendMessage(reportText);
     }
 }
